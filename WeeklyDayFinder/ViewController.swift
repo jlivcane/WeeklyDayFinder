1ecd443a16e3f66a2f69ecfa69c84ce5911c9afc
jlivcane <kalibrik@inbox.lv>
2020-08-26 11:45:56 +0300

    update

--- WeeklyDayFinder/ViewController.swift
+++ WeeklyDayFinder/ViewController.swift
@@ -8,59 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIApplicationDelegate {
-
+class ViewController: UIViewController{
+    
     @IBOutlet weak var dayTextField: UITextField!
     @IBOutlet weak var monthTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
     
     @IBOutlet weak var resultLabel: UILabel!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
 
-    func handleCalculation(){
-        let calendar = Calendar.current
-        
-        var dateComponent = DateComponents()
-        guard let day = dayTextField.text, let month = monthTextField.text, let year = yearTextField.text else {
-            print("textField err")
-            return
-        }
-    
-        dateComponent.day = Int(day)
-        dateComponent.month = Int(month)
-        dateComponent.year = Int(year)
-        
-        
-        guard let date = calendar.date(from: dateComponent) else {
-            print("calendar.date err")
-            return
-        }
-    
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_EN")
-        dateFormatter.dateFormat = "EEEE"
-        let weekday = dateFormatter.string(from: date)
-        let capitalizedWeekday = weekday.capitalized
-        resultLabel.text = capitalizedWeekday
-        
-        
-    }
-    
-    @IBAction func findDayTapped(_ sender: Any) {
-      handleCalculation()
-    }
-    
-    
+override func viewDidLoad() {
+       super.viewDidLoad()
 }
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.view.endEditing(true)
+}
+func handleCalculation(){
+       let calendar = Calendar.current
+       
+       var dateComponent = DateComponents()
+       guard let day = dayTextField.text, let month = monthTextField.text, let year = yearTextField.text else {
+           print("textField err")
+           return
+       }
+   
+       dateComponent.day = Int(day)
+       dateComponent.month = Int(month)
+       dateComponent.year = Int(year)
+       
+       
+       guard let date = calendar.date(from: dateComponent) else {
+           print("calendar.date err")
+           return
+       }
+let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "en_EN")
+    dateFormatter.dateFormat = "EEEE"
+    let weekday = dateFormatter.string(from: date)
+    let capitalizedWeekday = weekday.capitalized
+    resultLabel.text = capitalizedWeekday
+    
+    }
+    @IBAction func findTapped(_ sender: Any) {
+        handleCalculation()
+    }
+    
 
+}
